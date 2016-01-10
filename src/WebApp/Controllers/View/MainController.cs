4c6898bfa0d3e65658fdbbdb51825ccefb16f602
Gekctek <Gekctek@Gmail.com>
2016-01-10 12:52:17 -0800

    Adding WebApp

---
+++ src/WebApp/Controllers/View/MainController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers.View
+{
+	[Route("")]
+	public class MainController : ViewControllerBase
+	{
+		[HttpGet("")]
+		public IActionResult Index()
+		{
+			return this.View();
+		}
+	}
+}
---
+++ src/WebApp/Controllers/View/ViewControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers.View
+{
+	[Route("[controller]")]
+	public class ViewControllerBase : Controller
+	{
+
+	}
+}
