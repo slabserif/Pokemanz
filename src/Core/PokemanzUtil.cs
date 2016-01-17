e25fd5c1f8b9ef2ede3da549b8d8ff0bfc3c7b50
Gekctek <Gekctek@Gmail.com>
2016-01-17 13:12:47 -0800

    Changing spaces to tabs

--- src/Core/PokemanzUtil.cs
+++ src/Core/PokemanzUtil.cs
@@ -6,71 +6,75 @@ using System.Threading.Tasks;
 
 namespace Pokemanz.Core
 {
-    public static class PokemanzUtil
-    {
-        //TODO: Pokemon stat calculator
-        //Stat Research is derived from: http://bulbapedia.bulbagarden.net/wiki/Statistic
+	public static class PokemanzUtil
+	{
+		//TODO: Pokemon stat calculator
+		//Stat Research is derived from: http://bulbapedia.bulbagarden.net/wiki/Statistic
 
-        //METHOD: PokemonStatCalc();
-        //Runs a stat calculation 5 times, once for each stat (attack, defence, spattack, spdefense, speed)
-        //Runs an Hp calculation 
+		//METHOD: PokemonStatCalc();
+		//Runs a stat calculation 5 times, once for each stat (attack, defence, spattack, spdefense, speed)
+		//Runs an Hp calculation 
 
-        //Needs to receive 
-        //--int dv from DvRandomizer()
-        //--Level of current pokemon
-        //--Base Stats from tab delimited for pokemon
-        //--Count of total Evs earned between last level up and new level up
-        //--New moves if any (NOT MVP)
-        //--Evolution (NOT MVP)
-        //If hits level 100, cannot level anymore (NOT MVP)
+		//Needs to receive 
+		//--int dv from DvRandomizer()
+		//--Level of current pokemon
+		//--Base Stats from tab delimited for pokemon
+		//--Count of total Evs earned between last level up and new level up
+		//--New moves if any (NOT MVP)
+		//--Evolution (NOT MVP)
+		//If hits level 100, cannot level anymore (NOT MVP)
 
-        //QUESTIONS:
-        //--Is the stat calculation meant to add on to an existing stat, or replace it?
+		//QUESTIONS:
+		//--Is the stat calculation meant to add on to an existing stat, or replace it?
 
-        //Dv randomizer is used in generating the pokemon's initial stats, as well as being used in the level up formula produce diverse stats between two of the same pokemon at the same level. Dv's do not change after a pokemon has been initialized.
-        //Dv reserach is derived from: http://bulbapedia.bulbagarden.net/wiki/Individual_values
-        static int GetDeterminentValue()
-        {
-            Random random = new Random();
-            int dv = random.Next(0, 16);
-            return dv;
-        }
+		//Dv randomizer is used in generating the pokemon's initial stats, as well as being used in the level up formula produce diverse stats between two of the same pokemon at the same level. Dv's do not change after a pokemon has been initialized.
+		//Dv reserach is derived from: http://bulbapedia.bulbagarden.net/wiki/Individual_values
+		static int GetDeterminentValue()
+		{
+			Random random = new Random();
+			int dv = random.Next(0, 16);
+			return dv;
+		}
 
-        //Dv for hp is unique because it is calculated based on the results of the other stat dvs.
-        static int DeterminentRandomizerHp(int dvAttack, int dvDefense, int dvSpecial, int dvSpeed)
-        {
-            int dvHp = 0;
-            if(IsOdd(dvAttack)){
-                dvHp += 8;
-            }
-            if(IsOdd(dvDefense)){
-                dvHp += 4;
-            }
-            if(IsOdd(dvSpecial)){
-                dvHp += 1;
-            }
-            if(IsOdd(dvSpeed)){
-                dvHp += 2;
-            }
-            return dvHp;
-        }
+		//Dv for hp is unique because it is calculated based on the results of the other stat dvs.
+		static int DeterminentRandomizerHp(int dvAttack, int dvDefense, int dvSpecial, int dvSpeed)
+		{
+			int dvHp = 0;
+			if (IsOdd(dvAttack))
+			{
+				dvHp += 8;
+			}
+			if (IsOdd(dvDefense))
+			{
+				dvHp += 4;
+			}
+			if (IsOdd(dvSpecial))
+			{
+				dvHp += 1;
+			}
+			if (IsOdd(dvSpeed))
+			{
+				dvHp += 2;
+			}
+			return dvHp;
+		}
 
-        private static bool IsOdd(int value)
-        {
-            return value % 2 == 1;
-        }
+		private static bool IsOdd(int value)
+		{
+			return value % 2 == 1;
+		}
 
-        //SpriteSheet Front
+		//SpriteSheet Front
 
-        public static int GetFrontSpriteRow(int id)
-        {
-            return id / 16;
-        }
+		public static int GetFrontSpriteRow(int id)
+		{
+			return id / 16;
+		}
 
-        public static int GetFrontSpriteCol(int id)
-        {
-            return id % 16;
-        }
+		public static int GetFrontSpriteCol(int id)
+		{
+			return id % 16;
+		}
 
-    }
+	}
 }
--- src/Core/Pokemon.cs
+++ src/Core/Pokemon.cs
@@ -6,117 +6,117 @@ using System.Threading.Tasks;
 
 namespace Pokemanz.Core
 {
-    public class Pokemon
-    {
-        public string Name { get; private set; }
-        public int Id { get; private set; }
-        [PokemonProperty("Type 1")]
-        public PokemonType Type1 { get; private set; }
-        [PokemonProperty("Type 2")]
-        public PokemonType Type2 { get; private set; }
-        public Stat Hp { get; private set; }
-        public Stat Attack { get; private set; }
-        public Stat Defense { get; private set; }
-        public Stat SpAttack { get; private set; }
-        public Stat SpDefense { get; private set; }
-        public Stat Speed { get; private set; }
-        public int Experience { get; private set; }
-        public PokemonExpType ExpType { get; private set; }
-        public int BaseExpGiven { get; private set; }
+	public class Pokemon
+	{
+		public string Name { get; private set; }
+		public int Id { get; private set; }
+		[PokemonProperty("Type 1")]
+		public PokemonType Type1 { get; private set; }
+		[PokemonProperty("Type 2")]
+		public PokemonType Type2 { get; private set; }
+		public Stat Hp { get; private set; }
+		public Stat Attack { get; private set; }
+		public Stat Defense { get; private set; }
+		public Stat SpAttack { get; private set; }
+		public Stat SpDefense { get; private set; }
+		public Stat Speed { get; private set; }
+		public int Experience { get; private set; }
+		public PokemonExpType ExpType { get; private set; }
+		public int BaseExpGiven { get; private set; }
 
-        public bool AddExperience(int expEarned)
-        {
-            int levelBefore = GetLevel();
-            this.Experience += expEarned;
-            int levelAfter = GetLevel();
-            return levelAfter > levelBefore;
-        }
+		public bool AddExperience(int expEarned)
+		{
+			int levelBefore = GetLevel();
+			this.Experience += expEarned;
+			int levelAfter = GetLevel();
+			return levelAfter > levelBefore;
+		}
 
-        public int GetLevel()
-        {
-            switch (this.ExpType)
-            {
-                case PokemonExpType.MedFast:
-                    const double oneThird = 1.0 / 3.0;
-                    return (int)Math.Pow(this.Experience, oneThird);
-                case PokemonExpType.MedSlow:
-                    int level = 2;
-                    while (true)
-                    {
-                        int lvlCubed = (int)Math.Pow(level, 3);
-                        int expForLevelUp = (((6 / 5) * (lvlCubed)) - (15 * (level * level)) + (100 * level)) - 140;
-                        if (expForLevelUp > this.Experience)
-                        {
-                            return level - 1;
-                        }
-                        if (expForLevelUp == this.Experience)
-                        {
-                            return level;
-                        }
-                        level++;
-                    }
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(this.ExpType));
-            }
-        }
-    }
+		public int GetLevel()
+		{
+			switch (this.ExpType)
+			{
+				case PokemonExpType.MedFast:
+					const double oneThird = 1.0 / 3.0;
+					return (int)Math.Pow(this.Experience, oneThird);
+				case PokemonExpType.MedSlow:
+					int level = 2;
+					while (true)
+					{
+						int lvlCubed = (int)Math.Pow(level, 3);
+						int expForLevelUp = (((6 / 5) * (lvlCubed)) - (15 * (level * level)) + (100 * level)) - 140;
+						if (expForLevelUp > this.Experience)
+						{
+							return level - 1;
+						}
+						if (expForLevelUp == this.Experience)
+						{
+							return level;
+						}
+						level++;
+					}
+				default:
+					throw new ArgumentOutOfRangeException(nameof(this.ExpType));
+			}
+		}
+	}
 
-    public class Stat
-    {
-        public int BaseValue { get; private set; }
-        public int DeterminentValue { get; private set; }
-        public int StatExpValue { get; private set; }
+	public class Stat
+	{
+		public int BaseValue { get; private set; }
+		public int DeterminentValue { get; private set; }
+		public int StatExpValue { get; private set; }
 
-        protected int CalcStat(int level)
-        {
-            double newStat = Math.Floor(((this.BaseValue + this.DeterminentValue) * 2) + ((Math.Sqrt(this.StatExpValue) / 4) * level) / 100);
-            return (int)newStat;
-        }
+		protected int CalcStat(int level)
+		{
+			double newStat = Math.Floor(((this.BaseValue + this.DeterminentValue) * 2) + ((Math.Sqrt(this.StatExpValue) / 4) * level) / 100);
+			return (int)newStat;
+		}
 
-        public virtual int GetValue(int level)
-        {
-            int statValue = CalcStat(level);
-            statValue += 5;
-            return statValue;
-        }
-    }
+		public virtual int GetValue(int level)
+		{
+			int statValue = CalcStat(level);
+			statValue += 5;
+			return statValue;
+		}
+	}
 
-    public class HealthStat : Stat
-    {
-        public override int GetValue(int level)
-        {
+	public class HealthStat : Stat
+	{
+		public override int GetValue(int level)
+		{
 
-            int statValue = CalcStat(level);
-            statValue += level + 10;
-            return statValue;
-        }
-    }
+			int statValue = CalcStat(level);
+			statValue += level + 10;
+			return statValue;
+		}
+	}
 
 
-    public class PokemonPropertyAttribute : Attribute
-    {
-        public string Name { get; set; }
-        public PokemonPropertyAttribute(string name)
-        {
-            this.Name = name;
-        }
-    }
+	public class PokemonPropertyAttribute : Attribute
+	{
+		public string Name { get; set; }
+		public PokemonPropertyAttribute(string name)
+		{
+			this.Name = name;
+		}
+	}
 
-    public enum PokemonType
-    {
-        Grass,
-        Water,
-        Fire,
-        Flying,
-        Electric,
-        Normal,
-        Poison
-    }
+	public enum PokemonType
+	{
+		Grass,
+		Water,
+		Fire,
+		Flying,
+		Electric,
+		Normal,
+		Poison
+	}
 
-    public enum PokemonExpType
-    {
-        MedSlow,
-        MedFast
-    }
+	public enum PokemonExpType
+	{
+		MedSlow,
+		MedFast
+	}
 
 }
