b36b1e0e26ac59664d03a098215480c2f6217b5d
slabserif <slabserif3@gmail.com>
2016-01-23 18:26:56 -0800

    Added battle.cs
    battle.cs will get the players pokemon and battle a random pokemon, and
    facilitate turns.

---
+++ src/Core/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokemanz.Core
+{
+    public class Battle
+    {
+		public object GetPlayerPokemon(ref List<Pokemon> playerPokemonList)
+		{
+
+			return playerPokemonList[0];
+		}
+
+		PokemonExcelRepository.GetRandomPokemon();
+	}
+}
