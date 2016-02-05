145b9845e14d495d3e9d962117d569f5788ad7b5
slabserif <slabserif3@gmail.com>
2016-02-05 12:55:04 -0800

    Added method to split dialogue into screens

---
+++ src/Core/Utilities/UserInterfaceUtil.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokemanz.Core.Utilities
+{
+	public class UserInterfaceUtil
+	{
+
+		public static void DivideDialogPerScreen(string fullDialogue)
+		{
+			fullDialogue = "This is some dialogue. Really Really long dialogue.";
+
+			while (!String.IsNullOrWhiteSpace(fullDialogue))
+			{
+				int fullLength = fullDialogue.Length;
+				int maxLength = 20;
+				if (fullLength >= maxLength)
+				{
+					string truncatedDialogue = fullDialogue.Substring(0, maxLength);
+					if (truncatedDialogue.Contains(" "))
+					{
+						truncatedDialogue = truncatedDialogue.Substring(0, truncatedDialogue.LastIndexOf(" "));
+
+						if (String.IsNullOrWhiteSpace(truncatedDialogue))
+						{
+							Console.WriteLine("Empty string");
+							truncatedDialogue = fullDialogue.Substring(0, maxLength);
+							Console.WriteLine("Empty String Truncated Dialogue: " + truncatedDialogue + "\n");
+						}
+						Console.WriteLine("Truncated Dialogue: " + truncatedDialogue);
+
+						string nextScreenText = fullDialogue.Substring(truncatedDialogue.Length, fullLength - truncatedDialogue.Length);
+						Console.WriteLine("Next Screen Text: " + nextScreenText + "\n");
+						fullDialogue = nextScreenText;
+					}
+					else
+					{
+						Console.WriteLine("No Full words.");
+						string nextScreenText = fullDialogue.Substring(0, fullLength - maxLength);
+						Console.WriteLine("Next Screen Text: " + nextScreenText + "\n");
+						fullDialogue = nextScreenText;
+					}
+
+				}
+				else
+				{
+					Console.WriteLine("Not Truncated: " + fullDialogue + "\n");
+					fullDialogue = " ";
+				}
+
+			}
+			Console.WriteLine("No more text left.");
+		}
+
+	}
+}
